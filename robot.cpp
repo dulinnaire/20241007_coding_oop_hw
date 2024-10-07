c27578130fbc4ec9d04273115562f79b324bac3d
dulinnaire <150122917+dulinnaire@users.noreply.github.com>
2024-10-07 19:10:59 +0800

    为robot模块添加id health attack

--- robot.cpp
+++ robot.cpp
@@ -1,2 +1,63 @@
 #include "robot.h"
 
+Robot::Robot(int id): id(id) {}
+
+int Robot::get_id() const {
+    return id;
+}
+
+int Robot::get_health() const {
+    return health;
+}
+
+void Robot::set_health(int health) {
+    this->health = health;
+}
+
+int Robot::get_power() const {
+    return power;
+}
+
+void Robot::set_power(int power) {
+    this->power = power;
+}
+
+Hero::Hero(int id): Robot(id), health(500), power(120) {}
+
+std::string Hero::get_type() const {
+    return "Hero";
+}
+
+int Hero::get_attack() const {
+    return 50;
+}
+
+Engineer::Engineer(int id): Robot(id), health(250), power(150) {}
+
+std::string Engineer::get_type() const {
+    return "Engineer";
+}
+
+int Engineer::get_attack() const {
+    return 0;
+}
+
+Soldier::Soldier(int id): Robot(id), health(400), power(100) {}
+
+std::string Soldier::get_type() const {
+    return "Soldier";
+}
+
+int Soldier::get_attack() const {
+    return 10;
+}
+
+Sentinel::Sentinel(int id): Robot(id), health(400), power(100) {}
+
+std::string Sentinel::get_type() const {
+    return "Sentinel";
+}
+
+int Sentinel::get_attack() const {
+    return 10;
+}
--- robot.h
+++ robot.h
@@ -1,32 +1,95 @@
 #ifndef ROBOT_H
 #define ROBOT_H
 
+#include <string>
 // 机器人基类
-class Robot{
+class Robot {
 public:
+    Robot(int id);
+
+    virtual std::string get_type() const = 0;
+    virtual int get_attack() const = 0;
+
+    int get_health() const;
+    int get_power() const;
+    void set_health(int health);
+    void set_power(int power);
+
+    int get_id() const;
 
 private:
+    int id;
+    int health;
+    int power;
 };
 
 // 英雄机器人
-class Hero: public Robot{
+class Hero: public Robot {
+public:
+    Hero(int id);
 
+    virtual std::string get_type() const;
+    virtual int get_attack() const;
+    virtual int get_health() const;
+    virtual int get_power() const;
+
+    virtual void set_health(int health);
+    virtual void set_power(int power);
+
+private:
+    int health;
+    int power;
 };
 
 // 工程机器人
-class Engineer: public Robot{
+class Engineer: public Robot {
+    Engineer(int id);
 
+    virtual std::string get_type() const;
+    virtual int get_attack() const;
+    virtual int get_health() const;
+    virtual int get_power() const;
+
+    virtual void set_health(int health);
+    virtual void set_power(int power);
+
+private:
+    int health;
+    int power;
 };
 
 // 步兵机器人
-class Soldier: public Robot{
+class Soldier: public Robot {
+    Soldier(int id);
 
+    virtual std::string get_type() const;
+    virtual int get_attack() const;
+    virtual int get_health() const;
+    virtual int get_power() const;
+
+    virtual void set_health(int health);
+    virtual void set_power(int power);
+
+private:
+    int health;
+    int power;
 };
 
 // 哨兵机器人
-class Sentinel: public Robot{
+class Sentinel: public Robot {
+    Sentinel(int id);
 
+    virtual std::string get_type() const;
+    virtual int get_attack() const;
+    virtual int get_health() const;
+    virtual int get_power() const;
+
+    virtual void set_health(int health);
+    virtual void set_power(int power);
+
+private:
+    int health;
+    int power;
 };
-
 
 #endif
