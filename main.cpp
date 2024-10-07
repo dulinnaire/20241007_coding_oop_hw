cab32c19d15e26b77fad984b4ae209cc27b3af67
dulinnaire <150122917+dulinnaire@users.noreply.github.com>
2024-10-07 22:33:41 +0800

    添加main函数入口

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
+#include "field.h"
+#include "robot.h"
 #include <iostream>
 
-int main(){
-    return 0;
+int main() {
+    Hero h0 = Hero(0);
+    Engineer e1 = Engineer(1);
+    Soldier s2 = Soldier(2), s3 = Soldier(3), s4 = Soldier(4);
+    Sentinel se5 = Sentinel(5);
+    Robot* team[] = { &h0, &e1, &s2, &s3, &s4, &se5 };
+
+    Field f = Field(team);
+    while (true) {
+        for (int i = 0; i < 6; i++) {
+            f.get_robot(i)->action();
+        }
+    }
 }
