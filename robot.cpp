86ca4870899f3a08a8f8465e2e1aa06e49e43aa1
dulinnaire <150122917+dulinnaire@users.noreply.github.com>
2024-10-07 22:30:20 +0800

    完成Soldier和Sentinel的action函数

--- robot.cpp
+++ robot.cpp
@@ -20,8 +20,9 @@ int Robot::get_health() const {
 }
 
 void Robot::set_health(int health) {
-    if (health < 0) {
+    if (health <= 0) {
         this->health = 0;
+        std::cout << this->get_type() << this->get_id() << " is dead!" << std::endl;
     } else {
         this->health = health;
     }
@@ -57,7 +58,7 @@ void Hero::action() {
     int tmp = 0;
     ROBOT_ACTION act = EMPTY;
     while (!(act == MOVE || act == ATTACK)) {
-        std::cout << "Select action(1 for MOVE, 1 for ATTACK): " << std::endl;
+        std::cout << "Select action(1 for MOVE, 2 for ATTACK): " << std::endl;
         std::cin >> tmp;
         act = (ROBOT_ACTION)tmp;
     }
@@ -81,7 +82,8 @@ void Hero::action() {
                   && this->get_field()->get_robot(tmp)->get_health() > 0)
             )
             {
-                std::cout << "Select attack target(1 to 5): " << std::endl;
+                std::cout << "Select attack target(0 to 5, except for " << this->get_id()
+                          << "): " << std::endl;
                 std::cin >> tmp;
             }
             target = this->get_field()->get_robot(tmp);
@@ -177,7 +179,55 @@ int Soldier::get_attack() const {
     return 10;
 }
 
-void Soldier::action() {}
+void Soldier::action() {
+    // Print info
+    std::cout << std::endl;
+    std::cout << this->get_type() << "(" << this->get_id() << ") is acting" << std::endl;
+    std::cout << "HP: " << this->get_health() << std::endl;
+    std::cout << "Power: " << this->get_power() << std::endl;
+    std::cout << "Position: " << this->get_field()->get_pos(this) << std::endl;
+
+    // Select action
+    int tmp = 0;
+    ROBOT_ACTION act = EMPTY;
+    while (!(act == MOVE || act == ATTACK)) {
+        std::cout << "Select action(1 for MOVE, 2 for ATTACK): " << std::endl;
+        std::cin >> tmp;
+        act = (ROBOT_ACTION)tmp;
+    }
+
+    Robot* target;
+    // Perform action
+    switch (act) {
+        case MOVE:
+            tmp = -1;
+            while (!(0 <= tmp && tmp < this->get_field()->len())) {
+                std::cout << "Select moving(0 to 19): " << std::endl;
+                std::cin >> tmp;
+            }
+            std::cout << "Move to: position " << tmp << std::endl;
+            this->get_field()->move(this, tmp);
+            break;
+        case ATTACK:
+            tmp = -1;
+            while (
+                !((0 <= tmp && tmp < 6) && tmp != this->get_id()
+                  && this->get_field()->get_robot(tmp)->get_health() > 0)
+            )
+            {
+                std::cout << "Select attack target(0 to 5, except for " << this->get_id()
+                          << "): " << std::endl;
+                std::cin >> tmp;
+            }
+            target = this->get_field()->get_robot(tmp);
+            target->set_health(target->get_health() - this->get_attack());
+            std::cout << "Attack on: " << target->get_type() << target->get_id() << std::endl;
+            break;
+        default:
+            std::cout << "Invalid action" << std::endl;
+            break;
+    }
+}
 
 Sentinel::Sentinel(int id): Robot(id, 400, 100) {}
 
@@ -189,4 +239,52 @@ int Sentinel::get_attack() const {
     return 10;
 }
 
-void Sentinel::action() {}
+void Sentinel::action() {
+    // Print info
+    std::cout << std::endl;
+    std::cout << this->get_type() << "(" << this->get_id() << ") is acting" << std::endl;
+    std::cout << "HP: " << this->get_health() << std::endl;
+    std::cout << "Power: " << this->get_power() << std::endl;
+    std::cout << "Position: " << this->get_field()->get_pos(this) << std::endl;
+
+    // Select action
+    int tmp = 0;
+    ROBOT_ACTION act = EMPTY;
+    while (!(act == MOVE || act == ATTACK)) {
+        std::cout << "Select action(1 for MOVE, 2 for ATTACK): " << std::endl;
+        std::cin >> tmp;
+        act = (ROBOT_ACTION)tmp;
+    }
+
+    Robot* target;
+    // Perform action
+    switch (act) {
+        case MOVE:
+            tmp = -1;
+            while (!(0 <= tmp && tmp < this->get_field()->len())) {
+                std::cout << "Select moving(0 to 19): " << std::endl;
+                std::cin >> tmp;
+            }
+            std::cout << "Move to: position " << tmp << std::endl;
+            this->get_field()->move(this, tmp);
+            break;
+        case ATTACK:
+            tmp = -1;
+            while (
+                !((0 <= tmp && tmp < 6) && tmp != this->get_id()
+                  && this->get_field()->get_robot(tmp)->get_health() > 0)
+            )
+            {
+                std::cout << "Select attack target(0 to 5, except for " << this->get_id()
+                          << "): " << std::endl;
+                std::cin >> tmp;
+            }
+            target = this->get_field()->get_robot(tmp);
+            target->set_health(target->get_health() - this->get_attack());
+            std::cout << "Attack on: " << target->get_type() << target->get_id() << std::endl;
+            break;
+        default:
+            std::cout << "Invalid action" << std::endl;
+            break;
+    }
+}
