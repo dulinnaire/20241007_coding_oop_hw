cb0528b8908b19e77ac54dd6b0e706b22a8d2cb6
dulinnaire <150122917+dulinnaire@users.noreply.github.com>
2024-10-07 18:33:52 +0800

    添加robot，engine框架

---
+++ robot.h
@@ -0,0 +1,32 @@
+#ifndef ROBOT_H
+#define ROBOT_H
+
+// 机器人基类
+class Robot{
+public:
+
+private:
+};
+
+// 英雄机器人
+class Hero: public Robot{
+
+};
+
+// 工程机器人
+class Engineer: public Robot{
+
+};
+
+// 步兵机器人
+class Soldier: public Robot{
+
+};
+
+// 哨兵机器人
+class Sentinel: public Robot{
+
+};
+
+
+#endif
